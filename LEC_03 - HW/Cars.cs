e136994845d2f8fd8c1d13bea9fa2ce63a1f5127
mor <mor@DESKTOP-7VQJJUQ>
2022-11-05 11:50:36 +0200

    Add project files.

---
+++ LEC_03 - HW/Cars.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LEC_03___HW
+{
+    internal class Cars
+    {
+        public const int Number1 = 3;
+
+        public readonly int Number2 = 4;
+
+
+        public Cars()
+        {
+            Number2 = int.Parse(Console.ReadLine());
+        }
+
+        public void Check()
+        {
+            Console.WriteLine($"\nYour Age Is: {Number2}\n");
+            if (Number2 > 30)
+            {
+                Console.WriteLine($"{Number1}*{Number2} = {Number1 * Number2}");
+            }
+            else
+            {
+                Console.WriteLine($"{Number2}*20 = {Number2 * 20}");
+            }
+        }
+
+    }
+}
---
+++ LEC_03 - HW/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using LEC_03___HW;
+
+Console.WriteLine("const number = 3");
+Console.WriteLine($"ReadOnly number = 4\n");
+
+Console.WriteLine("Please Enter Your Age:");
+
+Cars cars = new Cars();
+
+cars.Check();
+
